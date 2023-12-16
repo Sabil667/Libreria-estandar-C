e28fb71db84cd0628c6034533113b71824bfbee0
Sabil667 <msabi@myuax.com>
2023-12-16 19:54:07 +0100

    .

--- archivos stdioh/1.cpp
+++ archivos stdioh/1.cpp
@@ -3,3 +3,11 @@
 int main() {
     // Nombre del archivo
     const char *nombreArchivo = "mi_archivo.txt";
+    // Intentar abrir el archivo en modo lectura ("r")
+    FILE *archivo = fopen(nombreArchivo, "r");
+
+    // Verificar si se pudo abrir el archivo
+    if (archivo == NULL) {
+        // Mostrar mensaje de error si el archivo no existe
+        printf("Error: No se pudo abrir el archivo '%s'\n", nombreArchivo);
+    } else {
